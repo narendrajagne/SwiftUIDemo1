6c048cf28c12f84e65496e9c27e953d879371c61
Narendra <narendra@ascratech.com>
2020-09-21 14:28:39 +0530

    initial commit

---
+++ AccessControl/AccessControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AccessControl
+//
+//  Created by Jagne on 21/09/20.
+//  Copyright © 2020 Jagne. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
